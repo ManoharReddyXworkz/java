aff3300d8942574e67ab84f13d46aaf42c2121e8
MANOHAR M <you@example.com>
2022-06-08 22:27:01 +0530

    Defaultconstructor

---
+++ Core_Java/src/com/xworkz/corejava/Constructor/Defaultconstructor/Gun.java
@@ -0,0 +1,18 @@
+package com.xworkz.corejava.Constructor.Defaultconstructor;
+
+public class Gun {
+  int noofBullet;
+  double weight;
+  String colour;
+  
+  Gun(){
+	  
+  }
+  Gun(int bullet,double wt,String clr){
+	  noofBullet =bullet;
+	  weight = wt;
+	  colour = clr;
+			  
+	  
+  }
+}
