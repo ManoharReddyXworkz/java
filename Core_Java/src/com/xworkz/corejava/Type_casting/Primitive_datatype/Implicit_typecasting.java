a617d7559dd709695ac1afc0c989b1f1091f97d5
MANOHAR M <you@example.com>
2022-06-15 00:02:04 +0530

    Type casting

---
+++ Core_Java/src/com/xworkz/corejava/Type_casting/Primitive_datatype/Implicit_typecasting.java
@@ -0,0 +1,31 @@
+package com.xworkz.corejava.Type_casting.Primitive_datatype;
+
+public class Implicit_typecasting {
+
+	public static void main(String[]args) {
+	//TODO Auto generated method stub
+		
+		byte a=10;
+		short s=a;
+		System.out.println("value of a is:"+a);
+		System.out.println("value of s is:"+s);
+		
+		int z=77;
+		long d = z;
+		
+		System.out.println("value of z is:"+z);
+		System.out.println("value of D is:"+d);
+		
+		long e = 77;
+		float f = e;
+		 
+		System.out.println("value of E is:"+e);
+		System.out.println("value of F is:"+f);
+		
+		float x = 77;
+		double g = z;
+		
+		System.out.println("value of X is:"+x);
+		System.out.println("value of G is:"+g);
+	}	
+}
---
+++ Core_Java/src/com/xworkz/corejava/Type_casting/non_primitive_datatype/nonprimativecastingdemo.java
@@ -0,0 +1,14 @@
+package com.xworkz.corejava.Type_casting.non_primitive_datatype;
+
+public class nonprimativecastingdemo {
+public static void main (String args[]) {
+	System.out.println("main started....");
+	
+	//upcasting
+	animal animal =new tiger();
+	
+	//down casting
+	tiger tiger=(tiger)animal;
+	System.out.println("mai9n ended...");
+}
+}
