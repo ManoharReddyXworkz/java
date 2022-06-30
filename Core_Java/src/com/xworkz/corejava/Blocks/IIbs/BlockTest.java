939ee5c639476ecdab0809c2caba02ab4d49f4e9
MANOHAR M <you@example.com>
2022-06-30 17:21:10 +0530

    iibs

---
+++ Core_Java/src/com/xworkz/corejava/Blocks/IIbs/BlockTest.java
@@ -0,0 +1,13 @@
+package com.xworkz.corejava.Blocks.IIbs;
+
+public class BlockTest {
+	
+	public static void main(String[] args) {
+		Television tv = new Television();
+		System.out.println("Brand Name is :" + tv.brand);
+		System.out.println("Type of the Tv is:"+ tv.type);
+		System.out.println("Price of Tv is:"+tv.price);
+		System.out.println("The Dimension:"+tv.dimension);
+	}
+
+}
---
+++ Core_Java/src/com/xworkz/corejava/Blocks/IIbs/Television.java
@@ -0,0 +1,21 @@
+package com.xworkz.corejava.Blocks.IIbs;
+
+public class Television {
+	String brand;
+	String type;
+	double price;
+	String dimension;
+
+	{
+	  this.brand ="LG";
+	  this.type = "LED";
+	  this.price = 2200000.55;
+	  this.dimension = "24.5inch";
+
+	}
+
+	Television(){
+		System.out.println("Hello this is constructor calling below Blocks!!!");
+	}
+
+}
